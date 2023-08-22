31ecf1b8dec45f73620f1160116cf51966d2891e
sakin <saklinemostaksakin58@gmail.com>
2023-08-22 06:42:46 +0600

    first commit

---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,18 @@
+import './Contact.scss'
+
+const Contact = () => {
+  return (
+    <>
+      <div className="contact">
+        <div className="container-head">
+            <div className="container">
+            <div className=''>Contact</div>
+            </div>
+        </div>
+        <div className="container-body"></div>
+      </div>
+    </>
+  )
+}
+
+export default Contact
--- src/pages/Routes/Routes.js
+++ src/pages/Routes/Routes.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../Home/Home";
 import Main from "../../layout/Main";
+import Contact from "../Contact/Contact";
 
 const routes = createBrowserRouter([
   {
@@ -10,7 +11,11 @@ const routes = createBrowserRouter([
         {
             path : '/',
             element : <Home></Home>
-        }
+        },
+        {
+          path : '/contact',
+          element : <Contact></Contact>
+      }
     ]
   },
 ]);
