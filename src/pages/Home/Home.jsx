db025871ffccd925452fe242e3011aa9da216e48
LeonAli5366 <leonali274784@gmail.com>
2023-08-25 03:24:25 +0600

    leon

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,8 @@
+import AboutAndContact from './About&Contact/AboutAndContact';
+import BgTitle from './Bg&Title/BgTitle';
 import CarCarousel from './CarCarousel/CarCarousel';
 import './Home.scss'
+import OurStories from './OurStories/OurStories';
 import VideoBannerSection from './VideoBannerSection/VideoBannerSection';
 
 const Home = () => {
@@ -7,6 +10,9 @@ const Home = () => {
         <div className='mb-96'>
             <VideoBannerSection></VideoBannerSection>
             <CarCarousel></CarCarousel>
+            <BgTitle/>
+            <OurStories/>
+            <AboutAndContact/>
         </div>
     );
 };
