5a11e4b364b7fdd553f4ea4fe4747fe39e63edcf
sakin <saklinemostaksakin58@gmail.com>
2023-08-27 20:53:18 +0600

    error with 1 problem

---
+++ src/assets/Callback.js
@@ -0,0 +1,17 @@
+import fbIcon from "./icons/fb-icon.svg";
+import instaIcon from "./icons/fb-icon.svg";
+import twitterIcon from "./icons/fb-icon.svg";
+import worldIcon from "./icons/fb-icon.svg";
+import ytIcon from "./icons/fb-icon.svg";
+import logo from './logo/logo.687a8a89821507df8003.png'
+import breadCrumb from './icons/breadCrumb.png'
+import battery from './product_img/battery.png'
+import solarCar from './images/solar.png'
+import jeepCar from './images/flux_car_jeep.png'
+import freedomCarPng from './images/flux_freedom_car-removebg-preview.png'
+import freedom_car from './images/flux_freedom_car.png'
+
+// video
+import carVideo from './images/car_inside.webp'
+
+export { fbIcon, instaIcon, twitterIcon, worldIcon, ytIcon, logo, breadCrumb, battery,solarCar,jeepCar, freedomCarPng, freedom_car};
