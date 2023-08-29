a1ac7c9c145bbc04c5403cac4e0a48d4508e9db7
MD BIPLOB HOSSAIN <108410378+Biplob17h@users.noreply.github.com>
2023-08-28 22:44:09 -0600

    about responseive complite

--- src/pages/About/AboutBannar/AboutBanner.jsx
+++ src/pages/About/AboutBannar/AboutBanner.jsx
@@ -4,8 +4,8 @@ import "./../About.scss";
 const AboutBanner = () => {
   return (
     <div className="AboutBannarBgImg">
-      <div className="flex justify-center pt-[18%]">
-        <h1 className="text-[#ddb655] text-5xl ">
+      <div className="md:flex md:justify-center pt-[60%] md:pt-[18%]">
+        <h1 className="text-[#ddb655] text-[30px] text-center  md:text-5xl">
           FLUX TECHNOLOGIES AFRICA
         </h1>
       </div>
