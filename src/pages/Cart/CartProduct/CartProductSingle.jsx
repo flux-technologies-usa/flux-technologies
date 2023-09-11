2615e2bc2c9bb0ad8e9977cb21bafd3d6a59a830
MD BIPLOB HOSSAIN <108410378+Biplob17h@users.noreply.github.com>
2023-09-10 22:37:29 -0600

    cart front end

--- src/pages/Cart/CartProduct/CartProductSingle.jsx
+++ src/pages/Cart/CartProduct/CartProductSingle.jsx
@@ -4,8 +4,8 @@ const CartProductSingle = ({ product }) => {
   console.log(product);
   const { img, name } = product.product;
   return (
-    <div className=" flex justify-around">
-      <div className=" flex">
+    <div className=" flex  mx-auto border1">
+      <div className=" flex w-4/12">
         <div className="w-[100px]">
           <img src={img} alt="" />
         </div>
@@ -14,15 +14,21 @@ const CartProductSingle = ({ product }) => {
           <div>Remove</div>
         </div>
       </div>
-      <div>
+      <div className="flex items-center w-4/12">
         <h1>Quantity :</h1>
-        <div>
-          <div>+</div>
-          <div>1</div>
-          <div>-</div>
+        <div className="flex pl-2">
+          <div className="w-[30px] h-[50px] customquantityBorder text-center text-[23px] font-extrabold pt-[6px]">
+            <span className="mt-20">+</span>
+          </div>
+          <div className="w-[30px] h-[50px] customquantityBorder text-center text-[20px] font-bold pt-[8px]">
+            1
+          </div>
+          <div className="w-[30px] h-[50px] customquantityBorder text-center text-[23px] font-extrabold pt-[6px]">
+            <span className="mt-20">-</span>
+          </div>
         </div>
       </div>
-      <div>
+      <div className="w-4/12">
         <h1>Price :</h1>
         <h1>1000.00</h1>
       </div>
