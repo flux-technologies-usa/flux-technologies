c6a74ede8c29d7fc67aa24511127bd6056afef2b
MD BIPLOB HOSSAIN <108410378+Biplob17h@users.noreply.github.com>
2023-10-07 01:48:17 -0600

    update story

---
+++ src/pages/Home/OurStories/OurStorySingleForBig.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "../Home.scss";
+
+const OurStorySingleForBig = ({ single }) => {
+  const { _id, title, subtitle } = single;
+
+  return (
+    <div>
+      <div className="hidden md:flex   select-none">
+        {/* start */}
+        <div className="h-[505px] w-[580px] relative pl-[40px] mb-[60px]">
+          <div className="flex justify-between">
+            <div className=" w-[500px] h-[340px] overflow-hidden">
+              <img
+                className="customStoryImg scale-[1.2]"
+                src={`http://localhost:8080/api/v1/ourstory/story-photo/${_id}`}
+                alt=""
+              />
+            </div>
+            <div className="h-[500px] w-[1px] bg-[#302f2f] ml-[40px]" />
+          </div>
+          <div className=" absolute top-[78%]">
+            <h1 className="text-[22px]">
+              {title}
+            </h1>
+            <h1 className="text-[17px] font-semibold mt-5">
+              {subtitle}
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OurStorySingleForBig;
