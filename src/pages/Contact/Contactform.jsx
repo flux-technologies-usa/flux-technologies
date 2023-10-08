1eefeb45982df7702dcff876ce3d252b59e781c2
MD BIPLOB HOSSAIN <108410378+Biplob17h@users.noreply.github.com>
2023-10-08 00:32:56 -0600

    profile update

--- src/pages/Contact/Contactform.jsx
+++ src/pages/Contact/Contactform.jsx
@@ -1,18 +1,46 @@
+import { useRef } from "react";
 import "./Contact.scss";
+import emailjs from "@emailjs/browser";
+import toast from "react-hot-toast";
 
 const Contactform = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_atxicih",
+        "template_nw7rxwm",
+        form.current,
+        "EcU50gpe_2wKn__oJ"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          toast.success("Message sent");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <div className="contactform w-full">
       <div className="flex gap-10">
-        <form className="flex flex-col gap-8 w-full mx-4 md:w-[500px] md:mx-0">
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className="flex flex-col gap-8 w-full mx-4 md:w-[500px] md:mx-0">
           <label className="flex flex-col mb-3">
-            <p className="text-white">First name</p>
-            <input type="text" placeholder="John" className=" py-4 px-6" />
-          </label>
-
-          <label className="flex flex-col mb-3">
-            <p className="text-white"> last name</p>
-            <input type="text" placeholder="Smith" className=" py-4 px-6" />
+            <p className="text-white">Your name</p>
+            <input
+              type="text"
+              name="user_name"
+              placeholder="John"
+              className=" py-4 px-6"
+            />
           </label>
 
           <label className="flex flex-col mb-3">
@@ -21,13 +49,16 @@ const Contactform = () => {
               type="email"
               placeholder="jsmith@gmail.com"
               className=" py-4 px-6 "
+              name="user_email"
             />
           </label>
           <label className="flex flex-col ">
+            <h1>Message</h1>
             <textarea
               rows="7"
               placeholder="Type your message here..."
               className=" py-4 px-6 "
+              name="message"
             />
           </label>
 
@@ -35,17 +66,23 @@ const Contactform = () => {
             <input type="checkbox" id="yes" name="yes" value="permittion" />
             <label for="yes" className="text-white">
               Join our mailing list for early access to product announcements,
-               special events in your area, promotions, and more.
+              special events in your area, promotions, and more.
             </label>
           </div>
 
           <button
             type="submit"
-            className="py-3 px-8 outline-none w-fit text-white font-bold border"
-          >
+            className="py-3 px-8 outline-none w-fit text-white font-bold border">
             Send Now
           </button>
-          <span className="text-white pt-[30px]  md:pb-[70px] text-[14px] font-[#dbd4c5]">By submitting this form, you agree to our <a href="http://www.fluxtechnologiesafrica.com/privacy" className="underline">Privacy Policy.</a></span>
+          <span className="text-white pt-[30px]  md:pb-[70px] text-[14px] font-[#dbd4c5]">
+            By submitting this form, you agree to our{" "}
+            <a
+              href="http://www.fluxtechnologiesafrica.com/privacy"
+              className="underline">
+              Privacy Policy.
+            </a>
+          </span>
         </form>
       </div>
     </div>
@@ -53,3 +90,57 @@ const Contactform = () => {
 };
 
 export default Contactform;
+
+<div className="contactform w-full">
+  <div className="flex gap-10">
+    <form className="flex flex-col gap-8 w-full mx-4 md:w-[500px] md:mx-0">
+      <label className="flex flex-col mb-3">
+        <p className="text-white">First name</p>
+        <input type="text" placeholder="John" className=" py-4 px-6" />
+      </label>
+
+      <label className="flex flex-col mb-3">
+        <p className="text-white"> last name</p>
+        <input type="text" placeholder="Smith" className=" py-4 px-6" />
+      </label>
+
+      <label className="flex flex-col mb-3">
+        <p className="text-white">email</p>
+        <input
+          type="email"
+          placeholder="jsmith@gmail.com"
+          className=" py-4 px-6 "
+        />
+      </label>
+      <label className="flex flex-col ">
+        <textarea
+          rows="7"
+          placeholder="Type your message here..."
+          className=" py-4 px-6 "
+        />
+      </label>
+
+      <div className="check flex gap-4">
+        <input type="checkbox" id="yes" name="yes" value="permittion" />
+        <label for="yes" className="text-white">
+          Join our mailing list for early access to product announcements,
+          special events in your area, promotions, and more.
+        </label>
+      </div>
+
+      <button
+        type="submit"
+        className="py-3 px-8 outline-none w-fit text-white font-bold border">
+        Send Now
+      </button>
+      <span className="text-white pt-[30px]  md:pb-[70px] text-[14px] font-[#dbd4c5]">
+        By submitting this form, you agree to our{" "}
+        <a
+          href="http://www.fluxtechnologiesafrica.com/privacy"
+          className="underline">
+          Privacy Policy.
+        </a>
+      </span>
+    </form>
+  </div>
+</div>;
