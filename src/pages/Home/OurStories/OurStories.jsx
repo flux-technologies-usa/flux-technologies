917d61755744d6476ef46c37833bde69ee5f196e
MD BIPLOB HOSSAIN <108410378+Biplob17h@users.noreply.github.com>
2023-10-08 22:44:14 -0600

    loader

--- src/pages/Home/OurStories/OurStories.jsx
+++ src/pages/Home/OurStories/OurStories.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import "./../Home.scss";
-import OurStoriesForBigDevice from "./OurStoriesForBigDevice";
-import OurStoriesForSmall from "./OurStoriesForSmall";
 import { useState } from "react";
 import { useEffect } from "react";
 import OurStorySingleForBig from "./OurStorySingleForBig";
+import ClipLoader from "react-spinners/ClipLoader";
 
 const OurStories = () => {
   const [ourStorys, setOurStorys] = useState([]);
+  const [loading, setloading] = useState(false);
   console.log(ourStorys);
 
   useEffect(() => {
+    setloading(true);
     fetch("https://flux-car.onrender.com/api/v1/ourstory/get-story")
       .then((res) => res.json())
-      .then((data) => setOurStorys(data.story));
+      .then((data) => {
+        setOurStorys(data.story);
+        setloading(false);
+      });
   }, []);
   return (
     <div className="px-2 md:px-16">
@@ -24,12 +28,18 @@ const OurStories = () => {
         <button className="customStoryButton ">View All</button>
       </div>
       <div className="flex overflow-auto customScroll">
-        {/* <OurStoriesForBigDevice></OurStoriesForBigDevice>
-        <OurStoriesForSmall></OurStoriesForSmall> */}
-        <div className="flex">
-          {ourStorys.map((single) => (
-            <OurStorySingleForBig key={single._id} single={single} />
-          ))}
+        <div className="w-full">
+          {loading ? (
+            <div className="overflow-hidden w-full  flex justify-center">
+              <ClipLoader color="#36d7b7" size={60} />
+            </div>
+          ) : (
+            <div className="flex">
+              {ourStorys.map((single) => (
+                <OurStorySingleForBig key={single._id} single={single} />
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </div>
